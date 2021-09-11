8f7cf1f8316f12ea2d72eb6a03f873f49dd33b8e
Stelan Briyan Simonsz <stelan@objectoneworld.com>
2021-09-11 10:47:28 +0530

    first commit

---
+++ src/main/java/com/aws/resourcesdemo/ResourcesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.aws.resourcesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourcesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourcesDemoApplication.class, args);
+	}
+
+}
